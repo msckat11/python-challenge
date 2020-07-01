41053b0a36cb19ea2a363bcd5e00053116650772
Katherine Schuman <katieschuman@katherinesair2.attlocal.net>
2020-07-01 17:16:51 -0400

    Put all profit/loss amounts into a list

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,37 @@
+# Import libraries
 import os
 import csv
 
+#
 file_path = os.path.join("Resources", "budget_data.csv")
 total_months = 0
 net_profit_loss = 0
 
+raw_pl_list = []
+pl_change_list = []
 
-with open(file_path) as csvfile:
-    csv_reader = csv.reader(csvfile, delimeter=",")
-    csv_header = next(csvfile)
-    print(f"Header: {csv_header})
 
-    # for row in csv_reader
+def pl_change(amt1, amt2):
+    monthly_pl = float(amt2) - float(amt1)
+    pl_change_list.append(monthly_pl)
+
+    
+    print("Financial Analysis")
+    print("---------------------------")
+
+with open(file_path, "r") as bankfile:
+    csv_reader = csv.reader(bankfile, delimiter=",")
+    csv_header = next(bankfile)
+    for row in csv_reader:
+        total_months = total_months + 1
+        net_profit_loss = net_profit_loss + float(row[1])
+        raw_pl_list.append(row[1])
+    print("Total Months: " + str(total_months))
+    print("Total Profit/Loss: $" + str(net_profit_loss))
+    
+
+
+
+    
+
+bankfile.close()
