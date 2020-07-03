b6d378db7a4e42d0295569233c3cf252e2d8b296
Katherine Schuman <katieschuman@katherinesair2.attlocal.net>
2020-07-03 14:40:02 -0400

    created dictionary matching months with profit/loss changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,9 +7,11 @@ file_path = os.path.join("Resources", "budget_data.csv")
 total_months = 0
 net_profit_loss = 0
 
+months = []
 raw_pl_list = []
 pl_list1 = []
 pl_list2 = []
+date_change_dict = {}
 
 print("Financial Analysis")
 print("---------------------------")
@@ -27,9 +29,15 @@ with open(file_path, "r") as bankfile:
         #Pull all profit/loss values from each month and put them in the raw_pl_list
         raw_pl_list.append(row[1])
 
+        #Pull each month's date and put them in the months list
+        months.append(row[0])
+
     
     #Check creation of raw list
     # print(raw_pl_list)
+
+    #Check creation of months list
+    # print(months)
     
     #Creating the first zip list from the raw data list
     pl_list1 = raw_pl_list.copy()
@@ -66,11 +74,25 @@ with open(file_path, "r") as bankfile:
     #Find the min value in pl_difference list
     pl_change_min = min(pl_difference)
 
+    #Copy months list and remove first month
+    dates = months.copy()
+    dates.pop(0)
+
+    # Check new dates list
+    # print(dates)
+
+    #Create dictionary with dates corresponding to profit/loss changes
+    for i in range(len(dates)):
+        date_change_dict[dates[i]] = pl_difference[i]
+
+    #Check dictionary creation
+    print(date_change_dict) 
+
     print("Total Months: " + str(total_months))
-    print("Total Profit/Loss: $" + str(net_profit_loss))
+    print("Total Profit/Loss: $" + str(round(net_profit_loss)))
     print("Average Change: $ " + str(pl_avg_change))
-    print("Greatest Increase in Profits: date here ($" + str(pl_change_max) + ")")
-    print("Greatest Decrease in Profits: date here ($" + str(pl_change_min) + ")")
+    print("Greatest Increase in Profits: date here ($" + str(round(pl_change_max)) + ")")
+    print("Greatest Decrease in Profits: date here ($" + str(round(pl_change_min)) + ")")
     
 
 bankfile.close()
