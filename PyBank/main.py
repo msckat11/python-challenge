d5fc4fc80b3e4f3747c3bb6762d5bf3406d1134a
Katherine Schuman <katieschuman@katherinesair2.attlocal.net>
2020-07-03 16:26:31 -0400

    created date/pl change dictionary. added dates to final message.

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,6 +12,7 @@ raw_pl_list = []
 pl_list1 = []
 pl_list2 = []
 date_change_dict = {}
+date_change_dict = dict()
 
 print("Financial Analysis")
 print("---------------------------")
@@ -82,17 +83,29 @@ with open(file_path, "r") as bankfile:
     # print(dates)
 
     #Create dictionary with dates corresponding to profit/loss changes
-    for i in range(len(dates)):
-        date_change_dict[dates[i]] = pl_difference[i]
+    for i in range(len(pl_difference)):
+        date_change_dict[pl_difference[i]] = dates[i]
 
     #Check dictionary creation
-    print(date_change_dict) 
+    # print(date_change_dict) 
+
+    #Call the key pair for pl_change_max
+    pl_change_max_date = date_change_dict[pl_change_max]
+
+    #Check the new variable for the date matching the max
+    # print(pl_change_max_date)
+
+    #Call the key pair for pl_change_min
+    pl_change_min_date = date_change_dict[pl_change_min]
+
+    #Check the new variable for the date matching the min
+    # print(pl_change_min_date)
 
     print("Total Months: " + str(total_months))
     print("Total Profit/Loss: $" + str(round(net_profit_loss)))
     print("Average Change: $ " + str(pl_avg_change))
-    print("Greatest Increase in Profits: date here ($" + str(round(pl_change_max)) + ")")
-    print("Greatest Decrease in Profits: date here ($" + str(round(pl_change_min)) + ")")
+    print("Greatest Increase in Profits: " + str(pl_change_max_date) + " ($" + str(round(pl_change_max)) + ")")
+    print("Greatest Decrease in Profits: " + str(pl_change_min_date) + " ($" + str(round(pl_change_min)) + ")")
     
 
 bankfile.close()
